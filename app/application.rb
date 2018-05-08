54d5f2dbcfe664a5d620da1a42db23efc6beeef4
Yina Zhu <yinagit@gmail.com>
2018-05-08 16:42:25 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 class Application
 
   def call(env)
     resp = Rack::Response.new
     now = Time.now
-
+    binding.pry
     if now.hour >=12
       resp.write "Good Afternoon"
     else
